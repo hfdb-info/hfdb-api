34518fd8249b46250ad2454604d2285358edde27
justin-blalock <113410576+justin-blalock@users.noreply.github.com>
2022-09-27 19:50:55 -0500

    Added Project Files

---
+++ src/main/java/info/hfdb/hfdbapi/HfdbApiApplication.java
@@ -0,0 +1,13 @@
+package info.hfdb.hfdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HfdbApiApplication.class, args);
+	}
+
+}
